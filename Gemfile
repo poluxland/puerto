72dd3acd615a41645e0dcd0263a11fd6e47bd1a2
Jose Jerez <joseignaciojerez@gmail.com>
2025-07-03 22:44:02 -0400

    port

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
 source "https://rubygems.org"
 
+# Gemfile
+gem "httparty"
+
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 8.0.2"
 # The modern asset pipeline for Rails [https://github.com/rails/propshaft]
--- app/controllers/port_controller.rb
+++ app/controllers/port_controller.rb
@@ -1,13 +1,21 @@
 class PortController < ApplicationController
   require "socket"
   require "timeout"
+  require "httparty"
 
   HOST    = "nexus.melon.cl"
   PORT    = 569
   TIMEOUT = 5  # segundos
+  URL     = "https://#{HOST}:#{PORT}/LlamadoCamiones/LlamadoLCA.aspx"
 
   def check
     @open = port_open?(HOST, PORT, TIMEOUT)
+
+    # Hacemos la petición HTTP y guardamos el HTML completo
+    response = HTTParty.get(URL, verify: false)
+    @html = response.body
+  rescue StandardError => e
+    @html = "<p style='color:red;'>Error al obtener HTML: #{e.message}</p>"
   end
 
   private
